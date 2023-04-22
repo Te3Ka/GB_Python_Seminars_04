25d8bad734bf9cd078ccf0f86f430d88e18ff1c9
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-04-22 10:41:51 +0300

    4 семинар. Подправлено 2-ое задание

--- Exercise01/main.py
+++ Exercise01/main.py
@@ -7,10 +7,10 @@ Input: She sells sea shells on the sea shore The shells that she sells are sea s
 So if sells sea shells on the sea shore I'm sure that the shells are sea shore shells
 Output: 13
 '''
-phrase = ("She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure. "
+phrase = ("She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure."
           "So if sells sea shells on the sea shore I'm sure that the shells are sea shore shells")
 print(phrase)
-phrase = phrase.replace('.', '')
+phrase = phrase.replace('.', ' ')
 list_phrase = phrase.lower().split(' ')
 set_phrase = set(list_phrase)
 print(len(set_phrase))
